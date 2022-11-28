919f870267e1ccd7c0fe774a9467b38e7c6c34e7
Livia Pires <livia.onofre@fatec.sp.gov.br>
2022-11-28 11:32:42 -0300

    Refatoração do código

--- REST_API/app.py
+++ REST_API/app.py
@@ -1,6 +1,6 @@
 import uuid
-from flask import Flask, request
-from flask_smorest import abort
+from flask import Flask, request, abort
+# from flask_smorest import abort
 from db import items, stores
 
 app = Flask(__name__)
@@ -36,6 +36,14 @@ def get_all_items():
 @app.post("/store")
 def create_store():
     store_data = request.get_json()
+
+    if "name" not in store_data:
+        abort(400, message="Bad request. Ensure 'name' is included in the JSON payload.")
+
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message="Store already exists.")
+
     store_id = uuid.uuid4().hex
     store = {**store_data, "id": store_id}
     stores[store_id] = store
@@ -55,6 +63,11 @@ def create_item():
             400, 
             message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.",
         )
+    for item in item.values():
+        if (
+            item_data["name"] == item["name"] and item_data["store_id"] == item["store_id"]
+        ):
+            abort(400, message="Item already exists.")
 
     if item_data["store_id"] not in stores:
         abort(404, message="Store not found.")
