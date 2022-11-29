5f9c6cde74c630471203c609e8a463648526545d
liviapires <livia.onofre@fatec.sp.gov.br>
2022-11-29 15:57:19 -0300

    Colocando docker e marshmallow pra funcionar

---
+++ REST_API/resources/item.py
@@ -0,0 +1,65 @@
+import uuid
+from flask import request
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from db import items
+from schemas import ItemsSchema, ItemUpdateSchema
+
+blp = Blueprint("items", __name__, description="Operations in items")
+
+import uuid
+from flask import request
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+
+from db import items
+
+blp = Blueprint("Items", "items", description="Operations on items")
+
+
+@blp.route("/item/<string:item_id>")
+class Item(MethodView):
+    def get(self, item_id):
+        try:
+            return items[item_id]
+        except KeyError:
+            abort(404, message="Item not found.")
+
+    def delete(self, item_id):
+        try:
+            del items[item_id]
+            return {"message": "Item deleted."}
+        except KeyError:
+            abort(404, message="Item not found.")
+
+
+    @blp.arguments(ItemUpdateSchema)
+    def put(self, item_data, item_id):
+        try:
+            item = items[item_id]
+            item |= item_data # |= é um operador de update de dict
+
+            return item
+        except KeyError:
+            abort(404, message="Item not found.")
+
+
+@blp.route("/item")
+class ItemList(MethodView):
+    def get(self):
+        return {"items": list(items.values())}
+
+    @blp.arguments(ItemsSchema)
+    def post(self, item_data):
+        for item in items.values():
+            if (
+                item_data["name"] == item["name"]
+                and item_data["store_id"] == item["store_id"]
+            ):
+                abort(400, message=f"Item already exists.")
+
+        item_id = uuid.uuid4().hex
+        item = {**item_data, "id": item_id}
+        items[item_id] = item
+
+        return item
--- REST_API/schemas.py
+++ REST_API/schemas.py
@@ -1,5 +1,9 @@
 from marshmallow import Schema, fields
 
+class StoreSchema(Schema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str(required=True)
+
 class ItemsSchema(Schema):
     id = fields.Str(dump_only=True)
     name = fields.Str(required=True)
@@ -9,7 +13,3 @@ class ItemsSchema(Schema):
 class ItemUpdateSchema(Schema):
     name = fields.Str()
     price = fields.Float()
-
-class StoreSchema(Schema):
-    id = fields.Str(dump_only=True)
-    name = fields.Str(required=True)
